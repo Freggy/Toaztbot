15945ee4f58a62946741efcd5ce52d49009f9afd
freggy <riegeryannic@gmail.com>
2018-01-04 23:34:26 +0100

    Bot should now be fully functional

--- Main.go
+++ Main.go
@@ -1,38 +1,71 @@
 package main
 
 import (
-	"github.com/knspriggs/go-twitch"
-	"fmt"
-	"time"
+    "fmt"
+    "github.com/onestay/go-new-twitch"
+    "github.com/bwmarrin/discordgo"
+    "os"
+    "time"
 )
 
+var (
+    twitchToken    string
+    discordToken   string
+    twitchChannel  string
+    discordChannel string
+)
+
+
 func main() {
+    
+    if len(os.Args) < 4 {
+        fmt.Println("Too few arguments. Expecting: <twitchToken> <discordToken> <twitchChannel> <discordChannel>")
+        return
+    }
+    
+    twitchToken    = os.Args[0]
+    discordToken   = os.Args[1]
+    twitchChannel  = os.Args[2]
+    discordChannel = os.Args[3]
+    
+    twitchSession       := twitch.NewClient(twitchToken)
+    discordSession, err := discordgo.New("Bot " + discordToken)
+    discordSession.Open()
+    
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    
+    defer discordSession.Close()
+    
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    
 	fmt.Println("Starting to watch for changes...")
-	session, err := twitch.NewSession(twitch.NewSessionInput{ClientID: "s5374c3j2vgdtl3gtbqxdla35gfvhv"})
-	
-	if err != nil {
-		panic(err)
-	}
-	
 	ticker := time.NewTicker(1 * time.Second)
 	
 	for {
 		select {
 		case <- ticker.C:
-			checkStream(session)
+			checkStream(twitchSession, discordSession)
 		}
 	}
 }
 
-// Checks if the stream is on.
-func checkStream(session *twitch.Session) {
-	stream, err := session.GetStream(&twitch.GetStreamsInputType{Channel: "ToaztyTV"})
+
+// Checks if the stream is on and sends message to specified Discord channel.
+func checkStream(tc *twitch.Client, ds *discordgo.Session) {
+	streamData, err := tc.GetStreams(twitch.GetStreamsInput{UserLogin: []string{twitchChannel}})
+	
 	if err != nil {
 		panic(err)
 	}
 	
-	if stream.Total == 1 {
+	if len(streamData) >= 1 {
 		fmt.Println("Channel is online, sending message to Discord channel...")
-		// TODO: output message to discord
+        ds.ChannelMessageSend(discordChannel, "Ich bin jetzt Live auf Twitch: \n`" + streamData[0].Title + "`\nhttps://www.twitch.tv/" + twitchChannel)
 	}
 }
